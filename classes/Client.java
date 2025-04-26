6ca50287623c37ed0601c5ea9b28272dea66ac9b
alitahiri1 <alitahiri.unv@gmail.com>
2025-04-26 13:58:26 +0100

    attribut Client et Demandeur

--- classes/Client.java
+++ classes/Client.java
@@ -1,5 +1,48 @@
 package recrutement.classes;
 
 public abstract class Client {
+    private int codecl;
+    private String adresse;
+    static protected int id=0;
     
+    private String tel;
+
+
+    public Client(int codecl, String adresse, String tel) {
+        this.codecl = id;
+        this.adresse = adresse;
+        this.tel = tel;
+    }
+
+    public int getCodecl() {
+        return codecl;
+    }
+
+    public void setCodecl(int codecl) {
+        this.codecl = codecl;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+    public static int getId() {
+        return ++id;
+    }
+
+    public static void setId(int id) {
+        Client.id = id;
+    }
+
 }
