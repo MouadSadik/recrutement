8d031ae0f24c2d48199cb8622730e037503239b7
MouadSadik <sadikmoad90@gmail.com>
2025-05-03 17:05:00 +0200

    Add EditionDAO

--- src/main/java/DAO/EditionDAO.java
+++ src/main/java/DAO/EditionDAO.java
@@ -2,37 +2,145 @@ package main.java.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import main.java.classes.Edition;
+import main.java.classes.Journal;
 import main.java.utils.DatabaseConnection;
 
 public class EditionDAO {
-    
-    public static boolean ajouterEdition(Edition edition){
+
+    public static boolean ajouterEdition(Edition edition) {
         String sql = "Insert into edition (codejournal, numedition, dateparution) values (?, ?, ?)";
 
-        try(Connection conn = DatabaseConnection.getConnection();
-            PreparedStatement statement = conn.prepareStatement(sql) ) {
-                statement.setInt(1, edition.getCodeJournal());
-                statement.setInt(2, edition.getNumEdition());
-                statement.setDate(3, java.sql.Date.valueOf(edition.getDateParution()));
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setInt(1, edition.getCodeJournal());
+            statement.setInt(2, edition.getNumEdition());
+            statement.setDate(3, java.sql.Date.valueOf(edition.getDateParution()));
 
-                int rowsInserted = statement.executeUpdate();
-                return rowsInserted>0;
+            int rowsInserted = statement.executeUpdate();
+            return rowsInserted > 0;
 
-            } catch(SQLException e) {
-                System.out.println("Erreur SQL: " + e.getMessage());
-                return false;
+        } catch (SQLException e) {
+            System.out.println("Erreur SQL (Ajout): " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean supprimerEdition(int codeJournal, int numEdition) {
+        String sql = "DELETE FROM edition where codeJournal = ? AND numedition = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setInt(1, codeJournal);
+            statement.setInt(2, numEdition);
+            int rowsDeleted = statement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            System.out.println("Erreur SQL (Delete) : " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static boolean modifierEdition(Edition edition) {
+        String sql = "UPDATE edition SET dateparution = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement statement = conn.prepareStatement(sql)) {
+            statement.setDate(1, java.sql.Date.valueOf(edition.getDateParution()));
+
+            int rowsUpdated = statement.executeUpdate();
+            return rowsUpdated > 0;
+        } catch (SQLException e) {
+            System.out.println("Erreur SQL (UPDATE) :" + e.getMessage());
+            return false;
+        }
+    }
+
+    public static Edition getEditionById(int codeJournal, int numEdition) {
+        String sql = "SELECT * FROM edition WHERE codejournal = ? AND numEdition = ?";
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, codeJournal);
+            stmt.setInt(2, numEdition);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+
+                java.sql.Date sqlDate = rs.getDate("dateparution");
+                LocalDate dateParution = sqlDate.toLocalDate();
+
+                return new Edition(
+                        rs.getInt("codejournal"),
+                        rs.getInt("numEdition"),
+                        dateParution);
+
             }
-        
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL (lecture par id) : " + e.getMessage());
+        }
+        return null;
+    }
+
+    public static List<Edition> getAllEditions() {
+        List<Edition> editions = new ArrayList<>();
+        String sql = "SELECT * FROM edition";
+        try (Connection conn = DatabaseConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet rs = stmt.executeQuery()) {
+            while (rs.next()) {
+                java.sql.Date sqlDate = rs.getDate("dateparution");
+                LocalDate dateParution = sqlDate.toLocalDate();
+                Edition e = new Edition(
+                        rs.getInt("codejournal"),
+                        rs.getInt("numEdition"),
+                        dateParution);
+
+                editions.add(e);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erreur SQL (lecture par id) : " + e.getMessage());
+        }
+        return editions;
     }
 
     public static void main(String[] args) {
+
         Edition edition1 = new Edition(2, 1, LocalDate.of(2025, 10, 10));
-        if(ajouterEdition(edition1)){
+        if (ajouterEdition(edition1)) {
             System.out.println("Edition Ajoute");
         }
+
+        Edition edition2 = new Edition(3, 3, LocalDate.of(2025, 10, 10));
+        if (ajouterEdition(edition2)) {
+            System.out.println("Edition Ajoute");
+        }
+
+        Edition edition3 = new Edition(3, 1, LocalDate.of(2025, 10, 10));
+        if (ajouterEdition(edition3)) {
+            System.out.println("Edition Ajoute");
+        }
+
+        if (supprimerEdition(1, 3)) {
+            System.out.println("Edition Supprimee");
+        }
+
+        Edition edition4 = getEditionById(1, 2);
+        if (edition4 != null) {
+            System.out.println("Edition Trouvee : " + edition4);
+        }
+
+        List<Edition> allEditions = getAllEditions();
+        System.out.println("Tous les editions :");
+        for (Edition e : allEditions) {
+            System.out.println(e);
+        }
+
     }
 }
